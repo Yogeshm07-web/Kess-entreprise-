3f2c5e0bc33a846492f584a28ae97fb086416623
y''m <yogeshmunsah07@gmail.com>
2024-04-22 10:07:19 +0400

    modif

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
+// App.js
 import React from 'react';
 import Navbar from './Navbar';
 import HomePage from './HomePage.js';
-
+import './App.css'; // Import the CSS file for styling
 
 const App = () => {
   return (
-    <div>
+    <div className="app-container">
       <Navbar />
       <HomePage />
-    
       {/* Other content of your application */}
     </div>
   );
 };
 
 export default App;
-
-
